6926d8f4ce4af7ef808ee8bb7b34ff11f43b3a93
Mélanie Delaville <melaniedelaville@gmail.com>
2025-05-07 15:09:35 +0000

    Version agrégée campaigns_day

--- models/int_campaigns.sql
+++ models/int_campaigns.sql
@@ -1,3 +1,5 @@
+WITH campaign_group AS(
+
 SELECT *
 from {{ref("stg_raw__adwords")}}
 UNION ALL
@@ -9,3 +11,14 @@ from {{ref("stg_raw__criteo")}}
 UNION ALL
 SELECT *
 from {{ref("stg_raw__facebook")}}
+
+)
+
+SELECT
+date_date,
+SUM(ads_cost) AS ads_cost,
+SUM(impression) AS ads_impression,
+SUM(click) AS ads_clicks,
+FROM campaign_group
+GROUP BY date_date
+ORDER BY date_date DESC
